07b1e098e037749c099d5c0a0dc5163f0381af84
Syeda Manaal Amir <manaalamir19@gmail.com>
2025-08-20 17:15:03 +0300

    Add files via upload

---
+++ Target.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody targetRb;
+    private float minSpeed = 14f;
+    private float maxSpeed = 17f;
+    private float maxTorque = 10f;
+    private float xRange = 4f;
+    private float ySpawnPos = -6f;
+    private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos,0);
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+    }
+}
